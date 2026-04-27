387c68fc67f7e072c9f1153bbc5fba2e1ad64a0f
peterluke101 <158350626+peterluke101@users.noreply.github.com>
2026-04-27 05:34:25 -0700

    fix: add case-study to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -8,5 +8,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "weekly",
       priority: 1,
     },
+    {
+      url: "https://peterlukedigital.com/case-study",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
   ];
 }
